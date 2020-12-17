55cbfe8aa6f68464cb037b1239cb9ba9f049ec84
Michael <Mick666@hotmail.com.au>
2020-12-18 00:10:21 +1100

    Now uses React frontend

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import diagnosisRouter from './routes/diagnosis';
 import patientRouter from './routes/patients';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../build')));
 
 const PORT = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../build'));
+});
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
